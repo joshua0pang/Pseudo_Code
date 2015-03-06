b2d61f14fc1392e92187f6542289c0f32981ce71
josh यशस्वी <hanners.pang@gmail.com>
2015-03-05 20:51:35 -0800

    A Myth About Computers

---
+++ Pseudo_Code.java
@@ -0,0 +1,226 @@
+Preface // Read Me
+
+"Welcome to Pseudo Code. In this novella, we'll be sharing small, relaxing tales which sneak in byte sized bits of computer science here and there. Enjoy!"
+
+// Chapter 1: The Flowers Of The Garden 
+// An Introduction to Loops 
+
+The Flowers Of The Garden live life in a cycle. They recycle. 
+Every day The Flowers play in sunlight, then dream in moonlight, then play in sunlight, then dream in moonlight. This cycle recycles forever. The Earth loves to recycle. 
+Every season The Flowers grow big, then grow small, then grow big, then grow small. This cycle recycles forever. The Earth loves to recycle.
+The Flowers of the Garden live life in a cycle. They recycle. 
+
+In computer science, we call this a loop.
+
+Moral Of The Story: Loops are how we recycle in computer science. Computers love to loop.
+
+// Chapter 2: Gardener Caroline 
+// Why we need structure
+
+Caroline tends her garden lovingly. She cares for each little flower individually, "Hello little flower, how are you this morning?" She smiles as she waters the thirsty plants. 
+Caroline is quite a good gardener. She has ever so many flowers! In fact, Caroline is getting overwhelmed by her success. 
+She needs to organize how she cares for her flowers, so she can give each one the care and love they deserve. "Oh little flowers, what will I do?" 
+That night, Caroline finds the solution she needs in a dream.
+
+Moral Of The Story: Gardener Caroline needs "data structures" to manage the plethora of flowers in her garden. In computer science, we need data structures to manage the plethora of binary code in our programs.
+
+// Chapter 3: Gardener Caroline Dreams A Solution 
+// An Introduction to Declaring and Instantiating Variables
+
+Caroline awakes to the first light of dawn. 
+Every morning Caroline does a few basic things the same way. Essentially, her morning routine is a loop. 
+The first thing she does every morning is yawn. After a good yawn, she stretches a nice long stretch. 
+Caroline greets her friend Mr. Sun the same way every morning, "Good morning, Mr. Sun!" 
+Caroline has greeted each day every day in this way all her life. This is her World Famous Caroline Morning Loop! "I wonder how The Flowers greet the new day?"
+Excited from her dream last night, Caroline jumps out of bed and runs to her friend Old Mr. Computer. She needs to Mr. Computer to remember the events of her dream because sometimes she forgets. Old Mr. Computer is Old Faithful when it comes to remembering things for our friend Gardener Caroline.
+
+In her dream, Caroline found herself upset in the middle of her garden. Suddenly, all the flowers gathered around her in a protective circle. 
+"Caroline, why are you upset? You give us so much water and food and love, what can we do to lift your spirit?" 
+Caroline replied, "Little flowers, I am worried about how I can take care of all of you... How can I love all of you equally?" 
+The Flowers said, "Well, maybe if you knew all of our names, you would be able account for all of us." 
+
+Caroline was very happy that The Flowers were speaking to her!!!
+
+A Blue Flower introduced himself, "My name is Mr. Blue. I'm ever so grateful for all of your help!"
+
+A Yellow Flower introduced herself, "My name is Ms. Yellow. Caroline, you make me glow whenever I see you!"
+
+Finally, A Green Flower introduced himself, "My name is Mr. Green. Caroline, perhaps you could tell Old Mr. Computer about our names when you wake up?"
+
+While chatting with Old Mr. Computer, Caroline thought of a way to organize all the names of all The Flowers Of The Garden. Ever since she was little, Caroline had tied colored strings around her fingers to keep track of important things she needed to remember. She tied a blue string around her first finger for Mr. Blue, a yellow string around her second finger for Ms. Yellow, and a green string around her third finger for Mr. Green.
+
+Remembering how she used to remember things, Caroline asked Old Mr. Computer to remember the names of The Flowers like this:
+
+Blue String = "Mr. Blue"
+Yellow String = "Ms. Yellow"
+Green String = "Mr. Green"
+
+Caroline simply cannot remember the last time she was this happy. The Flowers all introduced themselves to her!!! And they said they love everything about how she cares for them. She can hardly contain her glee.
+
+Moral Of The Story: Just as The Flowers declared their names, in computer science we need to have names for things as well. We call these names for things "variables". We call introducing names "declaring." Giving the name a "value" we call "initialization". But lets not get too technical, shall we?
+
+// Chapter 4: Caroline Finds Joy In Old Mr. Computer's Jargon
+// Correct String Syntax In Java
+
+Caroline, being a wonderful gardener, is used to interacting with bugs. Bees provide sweet music as they buzz around The Flowers. 
+
+Old Mr. Computer usually has a few bugs crawling around his circuits. Since Caroline was used to bugs in her garden, and they were minor, she let them be. Like bugs in her garden, the bugs crawling around in Old Mr. Computer may be useful in mysterious ways. The bugs caused Mr. Computer to remember her words like this:
+
+String Blue = "Mr. Blue";
+String Yellow = "Ms. Yellow";
+String Green = "Mr. Green";
+
+Caroline did not know why the some of the words got topsy-turvy, nor why a few friendly semicolons appeared, but so long as they were harmless, she was happy the way they were. "Nature is beautiful, and so are computers. All I have to do is see for myself!"
+Caroline loves Old Mr. Computer unconditionally. They have known each other all their lives. She has made getting to know his lingo — called Java — a creative project. Old Mr. Computer needs Caroline to help him communicate. He has many important things to say. Caroline never leaves a friend in need!
+
+Little did Caroline know, she was writing her first computer program in the modern programming language of Java. Java is the langauge Old Mr. Computer speaks. Many programming languages are similar in nature to Java. Java is the most popular programming langauge in computer science as of this writing. Knowing a bit of Java means knowing a bit of computer science in general. 
+
+Moral of the Story: Circa 2015, programming syntax is highly technical, unforgiving, and idiosyncratic. There is no getting around it. Caroline found joy in Java jargon by forgiving the rigidity of Old Mr. Computer. Can you?
+
+// Chapter 5: Caroline Chats With Old Mr. Computer
+// Indent Style In Java
+
+Every Wednesday for as long as she could remember, Caroline remembered to water Mr. Green.
+On one unusual Wednesday, Caroline plum forgot.
+Thursday morning, she found Mr. Green wilting with woe.
+"My dear little flower! Why do you wilt with woe?"
+"Oh Caroline, you did not visit me yesterday. I've have had nothing to drink, and I'm parched with thirst. I'm exhausted, and too dry even to cry."
+
+At that very moment, Caroline resolved herself to never again forget to feed any one of The Flowers. She went home that night to think with her whole entire brain in order to find a solution. Naturally, she found one.
+
+Many years ago, Caroline wore braces to straighten her teeth. Today, Caroline has the most beautiful smile for miles around. 
+
+Caroline was reminded of this when she was thinking about how to straighten out her problem. She realized Old Mr. Computer could organize her thoughts with opening "{" and closing "}" braces like this:
+
+Email from Old Mr. Computer:
+{
+	"Hi Caroline! Don't forget to feed The Flowers!"
+	"Your friend, Cornelius Computer."
+}
+
+Caroline, the very happy gardener that she was, wrote a schedule with the help of her friend Old Mr. Computer
+
+Schedule For My Little Flowers by Caroline
+{ 
+	If it is Monday, water "Mr. Blue"
+	If it is Tuesday, water "Ms. Yellow"
+	If it is Wednesday, water "Mr. Green"
+	If it is Thursday, read a book to The Flowers.
+	If it is Friday, sing a song to The Flowers.
+} 
+
+Caroline found the braces to be very useful in organizing her ideas. She started to use them anytime she worked with Old Mr. Computer, as it helped to keep both of them organized. Like the fence which protects The Garden, computer braces protect her ideas.
+
+Moral Of The Story: Everyone needs to structure their ideas. There are a limited number of conventions to begin and end a thought. In English, we use capital letters and periods. In Java, we use opening and closing braces. 
+
+// Chapter 6: Old Mr. Computer Finally Shares His Feelings
+// Algorithmic Pseudo Code For A Java Program
+
+Old Mr. Computer was feeling a little buggy as usual.
+One morning, Caroline was chatting with Mr. Computer and found out he had confused a few things. Being the kind and patient person she is, and given that his mistakes were harmless, she was happy the way they were. In fact, Old Mr. Computer is endearing to her heart because of his quirky glitches. With an air of humor, Caroline imagines it is her job to fix his faults. She does not take her job seriously, and solely focuses on having fun. This way, everyone wins.
+
+Mr. Computer had kept the braces like Caroline asked. However, shame-faced, Mr. Computer admitted he accidentally added a few things. It looked like this:
+
+ScheduleForMyLittleFlowers()
+{
+While(In The Garden) 
+{
+	If(Today == Monday)
+	{ 
+		Water(Mr. Blue);
+	}
+	Else If(Today == Tuesday)
+	{
+		Water(Ms. Yellow); 
+	}
+	Else If(Today == Wednesday)
+	{ 
+		Water(Mr. Green); 
+	}
+	Else If(Today == Thursday)
+	{
+		Read(A Book);
+	}
+	Else()	
+	{
+		Sing(A Song);
+	}
+}
+} 
+
+Gathering her energy, Caroline slowly and gradually asked Old Mr. Computer a few questions about the changes he had made.
+"Dear Mr. Computer, can you explain how you changed my schedule from English to Java?"
+"Well Caroline, if you think about it, Java and English are not that far apart. There are just a few grammatical differences, but essentially a sentence in English is a sentence in Java. If you can understand this, you'll go far. On Planet Computer, where I am from, we explain ourselves through terse interspersed comments around code, let's try that method."
+
+
+// Comments are denoted by the use of two forward slashes "//"
+
+Class(Caroline_Waters_The_Flowers)
+{ 
+
+// A class defines a program in Java. Let's say a program is like an novella.
+
+Function Schedule_For_My_Little_Flowers(Monday, Tuesday, Wednesday, Thursday, Friday) 
+{	
+
+// A function is a like a chapter of an novella. It usually contains a series of verbs — something the program does — and nouns — something the computer can use to do things — which 'return' a product — something worth the effort of doing the action of a verb with a noun. A return statement is the similar to  as using a paragraph or series of paragraphs to make a point, or prove a thesis. The return statement is the proven thesis. There may be many theses that need to be proven in order to prove a hierarchically larger thesis. This is mildly pyrimidal if you think about it, the bricks build higher until the apex.
+
+Function Water(A Flower)
+{
+	Return Pour Water On Said Flower;
+}
+
+// In a computer program, everything needs to be exactly explicitly defined. We might not think of the act of watering a flower as a function we perform, but to the tabula rasa computer, it needs just such precise instructions. Though this is almost universally tedious to regular folks, I think we forget that as children we needed to be taught everything from how to hold a toothbrush to sautee a flambe. We sublimate so many "functions" we forget that reality requires this attention to detail just as much as computer does. And, both are completely unforgiving. If I touch a hot stove, it will burn me. If I forget a semicolon, the compiler will burn me, so to speak.
+
+While(In The Garden) 
+// Syntax for a while loop. Parentheses are like the mouth of the function. They take in the food to allow the function to run. The technical term is "input."
+{
+	If(Today == Monday) // An "If" statement. Intuitive enough.
+	{ 
+		Return Water(Mr. Blue); //Return statements 
+	}
+	Else If(Today == Tuesday) // Nearly identical to the others. Only the variables change.
+	{
+		Return Water(Ms. Yellow); 
+	}
+	Else If(Today == Wednesday)
+	{ 
+		Return Water(Mr. Green); 
+	}
+	Else If(Today == Thursday)
+	{
+		Return Read(A Book);
+	}
+	Else()	
+	{
+		Return Sing(A Song);
+	}
+
+	// That seems fairly intuitive. Caroline taught us about braces, semicolons, all the jargon symbols of Java. The rest reads a bit like a book, don't you think? If Today is Monday, Water Mr. Blue. Simple. If you cannot see this, we have a problem. Seek technical support from a teacher.
+
+} // Closes the "while in the garden" loop
+} // Closes the "Schedule for my little flowers" function which not only details what to do when, but forces the program to actually do it. Caroline simply has to trust herself that she'll execute the actions she's set out for herself.
+} // Closes the class "Caroline waters the flowers."
+
+// Epilogue: Caroline Travels To Planet Computer
+// The Need For Immersion
+
+Caroline sighs to herself. "Mr. Computer, I doubt I will ever learn your language. We're just too different. I feel sad that I've myself resigned to this fate."
+Mr. Computer takes a minute to compute an answer. "Caroline, do you believe in magic?"
+An odd question coming from a computer.
+Caroline hesitantly replies, "Of course I do. Doesn't everybody?"
+Mr. Computer smiles, "Caroline, I think I know how we can bridge this gap. You're outgoing and friendly, at least that's what The Flowers tell me. Go find some people you enjoy spending time with who know Computer-ese. I think finding the right community is key, everything else file under magic. And do give yourself lots of time, be patient. Magic takes time. Please remember you don't need to be the Shapespeare of computer progrmaming either. Just basic literacy will do in this day and age. I know you can do it, my calculations are never wrong. Trust me."
+
+//Author's Endnote
+
+ || "In the day, in the night, say it right, say it all / "           ||
+ || "You either got it, or you don't, you either stand, or you fall." || 
+ 	- Say It Right, Nelly Furtado 
+
+ I think the problem Caroline faces in Psedo Code is completely legitimate. I face it daily. I did join a computer community, and learned a lot. I also found the community excessively aggressive, competitive, and threatening. Programming in this environment is unforgiving, adding insult to injury. More work needs to be done on the engineering side to allow for "good enough" progrmaming. People speak algorithmically, not programatically. They slang. However, regular people need to be a bit more organized in order to be responsible with increasingly powerful technology. Otherwise we will be a Wall-E society. So, both sides need to extend an olive branch. 
+
+I would like to see a genre of Natural Language Novels which introduce Computer Conventions slowly and gradually, and thoroughly. This is my modest, best foot forward contribution to such an effort. I am reminded of my other passion, Sanskrit. How remarkable words like karma, guru, charka, namaste have made their way powerfully into our daily life. However, "Return Statement" and "Indent Styles" and "Recursion" and "Iteration" are not in regular vocabulary by my estimation. We are the poorer for it. And, it is a relatively easy fix. I wrote the first draft of this novella in one work day for free as a class project. 
+
+Ultimately, the best incentive is fun. What could be more fun than bridging the gap between two worlds? Love is sort of the bridge between men and women, isn't it? Maybe fun is the bridge between computers and people. Something like that.
+
+—Josh Pang
