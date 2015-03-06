c1a8933c694ef001a3a9045d4f4b7b7f9708b6da
josh यशस्वी <hanners.pang@gmail.com>
2015-03-05 22:31:56 -0800

    Update Pseudo_Code.java

--- Pseudo_Code.java
+++ Pseudo_Code.java
@@ -292,10 +292,10 @@ literacy will do in this day and age. I know you can do it, my calculations are 
 I think the problem Caroline faces in Pseudo Code is completely legitimate. I face it daily. I did join a computer 
 community, and learned a lot. I also found the community excessively aggressive, competitive, and threatening. All
 Yang and no Yin. Programming an environment where mistakes are abundant, but each mistake inches you closer to death:
-this adds insult to injury. More work needs to be done on the engineering side to allow for "good enough" programming. 
-People speak algorithmically, not programatically. They slang. However, regular people need to be a bit more 
-organized in order to be responsible with increasingly powerful technology. Otherwise we will be a Wall-E society. 
-So, both sides need to extend an olive branch. 
+this adds insult to injury. More work needs to be done on the engineering side to allow for "good enough" 
+programming. People speak algorithmically, not programatically. They slang. However, regular people need to be a bit
+more organized in order to be responsible with increasingly powerful technology. Otherwise we will be a Wall-E 
+society. So, both sides need to extend an olive branch. 
 
 I would like to see a genre of literature like Pseudo Codewhich introduce computer science structures slowly and 
 gradually, and thoroughly. Pseudo Code is my modest, best foot forward contribution to such an effort. I am reminded 
