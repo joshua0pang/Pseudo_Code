b958e7ce20503a68decfea56e18ddd06421d387a
josh यशस्वी <hanners.pang@gmail.com>
2015-03-05 22:35:39 -0800

    Update Pseudo_Code.java

--- Pseudo_Code.java
+++ Pseudo_Code.java
@@ -291,7 +291,7 @@ literacy will do in this day and age. I know you can do it, my calculations are 
 
 I think the problem Caroline faces in Pseudo Code is completely legitimate. I face it daily. I did join a computer 
 community, and learned a lot. I also found the community excessively aggressive, competitive, and threatening. All
-Yang and no Yin. Programming an environment where mistakes are abundant, but each mistake inches you closer to death:
+Yang and no Yin. Programming an environment where mistakes are abundant, but each mistake inches you closer to shame:
 this adds insult to injury. More work needs to be done on the engineering side to allow for "good enough" 
 programming. People speak algorithmically, not programatically. They slang. However, regular people need to be a bit
 more organized in order to be responsible with increasingly powerful technology. Otherwise we will be a Wall-E 
